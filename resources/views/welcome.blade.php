bd83571fba7237466506706c63fa88bbb74ef0d0
luishdalgo <lhs0003@alu.medac.es>
2025-01-28 12:46:09 +0100

    CommitProduction

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <link rel="shortcut icon" href="{{asset('media/favicon.png')}}" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Home</title>
+    <title>Home</title> 
     @viteReactRefresh
     @vite(['resources/js/app.js'])
   </head>
