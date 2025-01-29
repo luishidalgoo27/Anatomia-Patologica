76ce322ebf7ad683a54017f650b48510630ee377
luishdalgo <lhs0003@alu.medac.es>
2025-01-29 18:46:33 +0100

    Commit Production

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 
     @viteReactRefresh
     @vite(['resources/js/app.js'])
-
+ 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
