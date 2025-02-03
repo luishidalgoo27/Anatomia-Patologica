0da7a75a7e13c3522e70d2a404f7b4d2005d0adf
luishdalgo <lhs0003@alu.medac.es>
2025-02-03 12:54:41 +0100

    Commit Production

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
