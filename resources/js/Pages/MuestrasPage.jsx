af37c89a4feb79d651fcd5746918c5e650299dbf
Rafael López Gómez <rlg0019@alu.medac.es>
2025-02-05 23:12:38 +0100

    cambios en MuestrasPage

--- resources/js/Pages/MuestrasPage.jsx
+++ resources/js/Pages/MuestrasPage.jsx
@@ -1,147 +1,76 @@
-import { useEffect, useState } from "react"
-
 export default function MuestrasPage(){
-    const [formatos, setFormatos] = useState([])
-    const [estudios, setEstudios] = useState([])
-    const [naturalezas, setNaturalezas] = useState([])
-    const [calidades, setCalidades] = useState([])
-
-    const [estudioIsSelected, setEstudioIsSelected] = useState('')
-
-
-    useEffect(()=>{
-        getFormato(),
-        getTipoEstudio(),
-        getNaturaleza(),
-        getCalidad()
-    }, [])
-
-
-    const getFormato = () => {
-        fetch(`/api/formato`)
-            .then(response => response.json())
-            .then(data => setFormatos(data))
-            .catch(error => console.error('Error al obtener los datos:', error))
-    }  
-
-    const getTipoEstudio = () => {
-        fetch(`/api/tipoEstudio`)
-            .then(response => response.json())
-            .then(data => setEstudios(data))
-            .catch(error => console.error('Error al obtener los datos:', error))
-    } 
-
-    const getNaturaleza = () => {
-        fetch(`/api/naturaleza`)
-            .then(response => response.json())
-            .then(data => setNaturalezas(data))
-            .catch(error => console.error('Error al obtener los datos:', error))
-    } 
-
-    const getCalidad = () => {
-        fetch(`/api/calidad`)
-            .then(response => response.json())
-            .then(data => setCalidades(data))
-            .catch(error => console.error('Error al obtener los datos:', error))
-    } 
-
     return(
         <>
             <div className="content-wrapper">
-                <div className="content">
+                <div className="content bg-slate-200">
 
-                    <div className="text-right p-3 pb-3">
+                <div className="pb-28"></div>
+                
+
+                <div className=" flex flex-col bg-white rounded-xl w-2/5 pt-10 pb-10  ml-auto mr-auto items-center gap-9 text-azulMedac">
+
+                            
+                    <div className="flex flex-col w-96 ">
+                        <label htmlFor="CodigoDeLaMuestra">Codigo de la muestra</label>
+                        <input name="CodigoDeLaMuestra" id="CodigoDeLaMuestra" type="text" className=" rounded-lg"/>
+                    </div>
+
+                    
+
+
+                
+                    <div className="flex flex-col w-96">
+                        <label htmlFor="fechaDeRecoleccion">Fecha de recolección</label>
+                        <input type="date" name="fechaDeRecoleccion" id="fechaDeRecoleccion" className=" rounded-lg"/>
+                    </div>
+
+                    <div className="flex flex-col w-96">
+                        <label htmlFor="formato">Formato</label>
+                        <select name="organo" id="organo" className=" rounded-lg"></select>                        
+                    </div>
+                    
+                    <div className="flex flex-col w-96">
+                        <label htmlFor="TipoDeNaturaleza">Tipo de naturaleza</label>
+                        <select name="TipoDeNaturaleza" id="TipoDeNaturaleza" className=" rounded-lg"></select>                        
+                    </div>
+
+                    <div className="flex flex-col w-96">
+                        <label htmlFor="organo">Órgano</label>
+                        <input id="organo" name="organo" type="text" className=" rounded-lg"/>
+                    </div>
+
+
+                
+                    <div className="flex flex-col w-96">
+                        <label htmlFor="TipoDeEstudio">Tipo de estudio</label>
+                        <select name="TipoDeEstudio" id="TipoDeEstudio" className=" rounded-lg"></select>                        
+                    </div>
+                    <div className="flex flex-col w-96">
+                        <label htmlFor="CalidadDeLaMuestra">Calidad de la muestra</label>
+                        <select name="CalidadDeLaMuestra" id="CalidadDeLaMuestra" className=" rounded-lg"></select>                        
+                    </div>
+
+
+                    <div className="flex flex-col  w-96">
+                        <label htmlFor="DescripcionDeCalidad">Descripción de calidad </label>
+                        <textarea name="DescripcionDeCalidad" id="DescripcionDeCalidad" className="h-36 rounded-lg"></textarea>
+                    </div>
+
+                    <div className="flex flex-col w-96">
+                        <label for="imagenesDeLaMuestra">Imagenes de la muestra</label>
+                        <input type="file" name="imagenesDeLaMuestra" id="imagenesDeLaMuestra" accept="image/*" multiple />
+                    </div>
+
+
+                </div>
+
+                <div className="text-right p-3 pb-3">
                         <button className="bg-azulMedac text-white w-36 h-12 rounded-lg ">
                             Siguiente
                         </button>
-                    </div>
+                </div>
 
 
-                    <div className="bg-white rounded-md h-4/5 pt-10 pb-10">
-                        <div className="flex flex-row flex-wrap ml-28 gap-10 ">
-                            <div className="flex gap-80">
-                                <div className="flex flex-col">
-                                    <label htmlFor="codigo">Codigo de la muestra</label>
-                                    <input id="codigo" name="codigo" type="text" className="w-44"/>
-                                </div>
-
-                                <div className="flex flex-col">
-                                    <label htmlFor="naturaleza">Tipo de naturaleza</label>
-                                    <select name="naturaleza" id="naturaleza" className="w-44">
-                                        {
-                                            naturalezas.map((naturaleza,index)=>(
-                                                <option key={index} value={naturaleza.id}>{naturaleza.nombre}</option>
-                                            ))
-                                        }
-                                    </select>                        
-                                </div>
-            
-
-                            </div>
-                            
-
-                            <div className="flex gap-80 flex-wrap">
-                                <div className="flex flex-col">
-                                    <label htmlFor="fecha">Fecha de recoleccion</label>
-                                    <input type="date" name="fecha" id="fecha" className="w-44"/>
-                                </div>
-
-                                <div className="flex flex-col">
-                                    <label htmlFor="formato">Formato</label>
-                                    <select name="formato" id="formato" className="w-44">
-                                        {
-                                            formatos.map((formato,index)=>(
-                                                <option key={index} value={formato.id}>{formato.nombre}</option>
-                                            ))
-                                        }
-                                    </select>                        
-                                </div>
-                                
-                                <div className="flex flex-col">
-                                    <label htmlFor="organo">Órgano</label>
-                                    <input id="organo" name="organo" type="text" className="w-44"/>
-                                </div>
-                            </div>
-                            
-
-                            <div className="flex gap-80 flex-wrap">
-                                <div className="flex flex-col">
-                                    <label htmlFor="estudio">Tipo de estudio</label>
-                                    <select name="estudio" id="estudio" className="w-44">
-                                        {
-                                            estudios.map((estudio,index)=>(
-                                                <option key={index} value={estudio.id}>{estudio.nombre}</option>
-                                            ))
-                                        }
-                                    </select>                        
-                                </div>
-                                <div className="flex flex-col">
-                                    <label htmlFor="calidad">Calidad de la muestra</label>
-                                    <select name="calidad" id="calidad" className="w-44">
-                                        {   
-                                            calidades.map((calidad,index)=>(
-                                                
-                                                <option key={index} value={calidad.id}>{calidad.nombre}</option>
-                                            ))
-                                        }    
-                                    </select>                        
-                                </div>
-                            </div>
-                        </div>
-                        
-
-                        <div className="flex flex-wrap  gap-10 pt-10">
-                            <div className="flex flex-col mr-auto ml-28">
-                                <label htmlFor="imageFile">Imagenes de la muestra</label>
-                                <input type="file" id="imageFile" accept="image/*" multiple />
-                            </div>
-
-                            <div className="flex flex-col mr-32">
-                                <label htmlFor="descripcion">Descripcion de calidad </label>
-                                <textarea name="" id="descripcion" className="h-36"></textarea>
-                            </div>
-                        </div>
-                    </div>
 
                 </div>
             </div>
