1c47743274c83353bb97d3d2ea5303251b7b197e
luishdalgo <lhs0003@alu.medac.es>
2025-02-06 15:23:50 +0100

    Commit Production

--- resources/js/Pages/TablaMuestras.jsx
+++ resources/js/Pages/TablaMuestras.jsx
@@ -73,7 +73,7 @@ export default function TablaMuestras() {
                                     </th>
                                 </tr>
                             </thead>
-
+ 
                             <tbody>
                                 {
                                     muestras.map((muestra,index) => (
