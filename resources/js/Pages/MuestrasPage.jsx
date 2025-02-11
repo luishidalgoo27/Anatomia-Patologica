a12b595ba67362d73943eb526661e0e6a9410f50
pedroangelbl <pedroangelbeltranlopez@gmail.com>
2025-02-11 18:52:27 +0100

    tabla muestras mas añadir con sweet alert

--- resources/js/Pages/MuestrasPage.jsx
+++ resources/js/Pages/MuestrasPage.jsx
@@ -1,14 +1,16 @@
 import { addMuestra } from "@/utils/muestrasApiRest"
 import { useEffect, useState } from "react"
 import Swal from "sweetalert2"
+import { handleAdd } from "@/utils/muestrasCrud"
 
-export default function MuestrasPage(){
+export default function MuestrasPage(){    
     //Estados para pintar los input de muestras
+    const [muestras,setMuestras] = useState([])
     const [formatos, setFormatos] = useState([])
     const [estudios, setEstudios] = useState([])
     const [naturalezas, setNaturalezas] = useState([])
     const [calidades, setCalidades] = useState([])
-
+    
     //Estados para añadir una muestra
     const [codigo,setCodigo] = useState('')
     const [fecha,setFecha] = useState('')
@@ -18,62 +20,23 @@ export default function MuestrasPage(){
     const [estudio, setEstudio] = useState('')
     const [id_calidad, setid_Calidad] = useState('')
     const [descripcion_calidad, setDescripcion_calidad] = useState('')
-
-
+    
+    
     useEffect(()=>{
+        getMuestras(),
         getFormato(),
         getTipoEstudio(),
         getNaturaleza(),
         getCalidad()
     }, [])
-
-
-    const getFormato = () => {
-        fetch(`/api/api/formato`)
-            .then(response => response.json())
-            .then(data => setFormatos(data))
-            .catch(error => console.error('Error al obtener los datos:', error))
-    }  
-
-    const getTipoEstudio = () => {
-        fetch(`/api/api/tipoEstudio`)
-            .then(response => response.json())
-            .then(data => setEstudios(data))
-            .catch(error => console.error('Error al obtener los datos:', error))
-    } 
-
-    const getNaturaleza = () => {
-        fetch(`/api/api/naturaleza`)
-            .then(response => response.json())
-            .then(data => setNaturalezas(data))
-            .catch(error => console.error('Error al obtener los datos:', error))
-    } 
-
-    const getCalidad = () => {
-        fetch(`/api/api/calidad`)
-            .then(response => response.json())
-            .then(data => setCalidades(data))
-            .catch(error => console.error('Error al obtener los datos:', error))
-    } 
-
-    const handleAddMuestra = async () => {
-        const nuevaMuestra = {
-            codigo,
-            fecha,
-            organo,
-            id_formato,
-            id_tipo_naturaleza,
-            id_calidad,
-            descripcion_calidad
-        };
     
-        try {
-            await addMuestra(nuevaMuestra);
-        } catch (error) {
-            console.error("Error al añadir la muestra:", error);
-            Swal.fire("Cancelled!", "Error al añadir la muestra", "error")
-        }
-    };
+    const getMuestras = () => {
+        fetch(`/api/muestras`)
+        .then(response => response.json())
+        .then(data => setMuestras(data))
+        .catch(error => console.error('Error al obtener los datos:', error))
+    }  
+    
 
     return(
         <>
@@ -82,6 +45,13 @@ export default function MuestrasPage(){
 
                     <div className="pb-28"></div>
                     
+                    <div>
+                        <div className="text-right p-3 pb-3">
+                            <button onClick={handleAdd} className="bg-azulMedac text-white w-36 h-12 rounded-lg ">
+                                Añadir muestra
+                            </button>
+                        </div>
+                    </div>
 
                     <div className="flex flex-col bg-white rounded-xl w-2/5 pt-10 pb-10 ml-auto mr-auto items-center gap-9 text-azulMedac">          
                         <div className="flex flex-col w-96 ">
@@ -157,12 +127,116 @@ export default function MuestrasPage(){
                         </div>
                     </div>
 
-                    <div className="text-right p-3 pb-3">
-                        <button onClick={handleAddMuestra} className="bg-azulMedac text-white w-36 h-12 rounded-lg ">
-                            Siguiente
-                        </button>
+                    {/* Tabla de muestras */}                    
+                    <div className="relative flex flex-col w-full h-full text-gray-700 bg-white shadow-md rounded-xl bg-clip-border">
+                        <table className="w-full text-left table-auto min-w-max">
+                            <thead>
+                                <tr>
+                                    <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                                        <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                                            Código
+                                        </p>
+                                    </th>
+                                    <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                                        <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                                            Fecha
+                                        </p>
+                                    </th>
+                                    <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                                        <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                                            Descripción
+                                        </p>
+                                    </th>
+                                    <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                                        <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                                            Calidad
+                                        </p>
+                                    </th>
+                                    <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                                        <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                                            Tipo de naturaleza
+                                        </p>
+                                    </th>
+                                    <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                                        <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                                            Órgano
+                                        </p>
+                                    </th>
+                                    <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                                        <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                                            Formato
+                                        </p>
+                                    </th>
+                                    <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                                        <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                                            Modificar
+                                        </p>
+                                    </th>
+                                    <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                                        <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                                            Eliminar
+                                        </p>
+                                    </th>
+                                </tr>
+                            </thead>
+ 
+                            <tbody>
+                                {
+                                    muestras.map((muestra,index) => (
+                                        <tr id={muestra.id} key={index}>
+                                            <td className="p-4 border-b border-blue-gray-50">
+                                                <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                                                    {muestra.codigo}
+                                                </p>
+                                            </td>
+                                            <td className="p-4 border-b border-blue-gray-50">
+                                                <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                                                    {muestra.fecha}
+                                                </p>
+                                            </td>
+                                            <td className="p-4 border-b border-blue-gray-50">
+                                                <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                                                    {muestra.descripcion_calidad}
+                                                </p>
+                                            </td>
+                                            <td className="p-4 border-b border-blue-gray-50">
+                                                <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                                                    {muestra.id_calidad}
+                                                </p>
+                                            </td>
+                                            <td className="p-4 border-b border-blue-gray-50">
+                                                <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                                                    {muestra.id_tipo_naturaleza}
+                                                </p>
+                                            </td>
+                                            <td className="p-4 border-b border-blue-gray-50">
+                                                <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                                                    {muestra.organo}
+                                                </p>
+                                            </td>
+                                            <td className="p-4 border-b border-blue-gray-50">
+                                                <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                                                    {muestra.id_formato}
+                                                </p>
+                                            </td>
+                                            <td className="p-4 border-b border-blue-gray-50">
+                                                <button id={muestra.id} onClick={()=> {handleUpdateMuestra(muestra,getMuestras)}} className="bg-blue-600 text-white w-36 h-12 rounded-lg ">
+                                                    Editar muestra
+                                                </button>
+                                            </td>
+                                            <td className="p-4 border-b border-blue-gray-50">
+                                                <button id={muestra.id} onClick={()=> {deleteMuestra(muestra.id,getMuestras)}} className="bg-blue-600 text-white w-36 h-12 rounded-lg ">
+                                                    Borrar muestra
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
                     </div>
 
+    
                 </div>
             </div>
             
