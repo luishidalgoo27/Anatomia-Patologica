8cb77731c7c03a715f6a21675d756a7e1955e433
luishdalgo <lhs0003@alu.medac.es>
2025-02-20 13:31:54 +0100

    CommitProduction

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
   
   <body class="hold-transition sidebar-mini">
     <div class="wrapper" id="root">
- 
+  
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
