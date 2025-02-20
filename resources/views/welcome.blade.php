f00581a34eafb96e7536934591707d2aa93ef26e
luishdalgo <lhs0003@alu.medac.es>
2025-02-20 18:11:57 +0100

    Commit Production

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
     <title>Fundación Davante</title>
- 
+  
     @viteReactRefresh
     @vite(['resources/js/app.jsx', 'resources/css/app.css']) 
      
